7029442220fd2f24f689a3da8464dac1ff925946
José Duarte <duarte.gmj@gmail.com>
2023-09-23 23:13:57 +0100

    Move querying to the Rust side

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,15 +1,54 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
+use base64::Engine;
+use serde_json::json;
+
 // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
 #[tauri::command]
-fn greet(name: &str) -> String {
-    format!("Hello, {}! You've been greeted from Rust!", name)
+async fn generate_image(api_token: String, prompt: String) -> String {
+    let client = reqwest::Client::new();
+    let response = client
+        .post("https://api.openai.com/v1/images/generations")
+        .bearer_auth(api_token)
+        .header("Content-Type", "application/json")
+        .body(
+            json!({
+                "prompt": prompt,
+                "n": 1,
+                "size": "1024x1024"
+            })
+            .to_string(),
+        )
+        .send()
+        .await
+        .unwrap();
+
+    // Really bad stripping
+    let image_url_json =
+        serde_json::from_slice::<serde_json::Value>(&response.bytes().await.unwrap()).unwrap()
+            ["data"][0]["url"]
+            .to_string();
+    let image_url = image_url_json.trim_matches('"');
+
+    println!("{}", image_url);
+
+    let image = reqwest::get(image_url)
+        .await
+        .unwrap()
+        .bytes()
+        .await
+        .map(|bytes| bytes.to_vec())
+        .unwrap();
+
+    let engine = base64::engine::general_purpose::STANDARD_NO_PAD;
+
+    engine.encode(image)
 }
 
 fn main() {
     tauri::Builder::default()
-        .invoke_handler(tauri::generate_handler![greet])
+        .invoke_handler(tauri::generate_handler![generate_image])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
--- src/App.svelte
+++ src/App.svelte
@@ -1,4 +1,5 @@
 <script lang="ts">
+  import { invoke } from "@tauri-apps/api/tauri";
   import { Button, Textarea, Input, Label } from "flowbite-svelte";
   import { LockSolid } from "flowbite-svelte-icons";
   let apiKey = "";
@@ -16,24 +17,31 @@
       return;
     }
 
-    const openai = new OpenAI({
-      apiKey: apiKey,
-      dangerouslyAllowBrowser: true,
-    });
-    const image = await openai.images.generate({
-      prompt: prompt,
-    });
-    console.log(image.data);
-    if (image.data[0].url) {
-      imageSource = image.data[0].url;
-    }
+    // const openai = new OpenAI({
+    //   apiKey: apiKey,
+    //   dangerouslyAllowBrowser: true,
+    // });
+    // const image = await openai.images.generate({
+    //   prompt: prompt,
+    // });
+
+    // Decent sidestep but probably not amazing
+    invoke("generate_image", { apiToken: apiKey, prompt: prompt })
+      .then((b64Image) => {
+        console.log(b64Image);
+        imageSource = `data:image/png;base64, ${b64Image}`;
+      })
+      .catch((error) => console.error(error));
     prompts = [prompt, ...prompts];
     prompt = "";
   }
 </script>
 
 <main class="flex-column h-screen w-screen" style="min-width:800px;">
-  <header class="h-[5vh] flex p-4 items-center">
+  <header
+    class="h-[5vh] flex p-4 items-center border-b"
+    style="min-height:48px"
+  >
     <h1 class="text-xl flex-1">PromptManager</h1>
 
     <div class="flex items-center space-x-4">
@@ -51,8 +59,8 @@
     </div>
   </header>
   <div class="h-[95vh] flex flex-col pb-4 pr-4 pl-4">
-    <div class="flex flex-1 overflow-hidden">
-      <div class="flex-1 overflow-auto" style="min-width:40%;">
+    <div class="flex flex-1 overflow-hidden space-x-4">
+      <div class="flex-1 overflow-auto" style="min-width:30%; max-width:50%;">
         {#each prompts as prompt, idx}
           <p class="p-2" style="word-wrap: break-word; white-space: pre-line;">
             {prompt}
@@ -62,17 +70,17 @@
           {/if}
         {/each}
       </div>
-      <div>
+      <div class="flex place-content-center" style="max-width:50%">
         {#if imageSource != ""}
           <img
-            style="min-width:256px; max-width=1024px;"
+            class="aspect-square object-contain rounded-lg"
             src={imageSource}
             alt="Prompt Result"
           />
         {/if}
       </div>
     </div>
-    <div class="flex items-center space-x-4">
+    <div class="flex items-center space-x-4" style="min-height:64px">
       <Textarea
         bind:value={prompt}
         class="flex-1"
