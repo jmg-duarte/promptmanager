f7f905f409d6121cd03cc46100a1e20fb12cc810
José Duarte <duarte.gmj@gmail.com>
2023-09-25 11:03:56 +0100

    Fixed the main interface issue

--- src/App.svelte
+++ src/App.svelte
@@ -1,6 +1,5 @@
 <script lang="ts">
   import { invoke } from "@tauri-apps/api/tauri";
-  import { Body, Client, ResponseType, getClient } from "@tauri-apps/api/http";
   import { Button, Textarea, Input, Label } from "flowbite-svelte";
   import { LockSolid } from "flowbite-svelte-icons";
 
@@ -16,19 +15,19 @@
       return;
     }
 
-    // invoke("generate_image", { apiToken: apiKey, prompt: prompt })
-    //   .then((b64Image) => {
-    //     console.log(b64Image);
-    //     imageSource = `data:image/png;base64, ${b64Image}`;
-    //   })
-    //   .catch((error) => console.error(error));
+    invoke("generate_image", { apiToken: apiKey, prompt: prompt })
+      .then((b64Image) => {
+        console.log(b64Image);
+        imageSource = `data:image/png;base64, ${b64Image}`;
+      })
+      .catch((error) => console.error(error));
 
     prompts = [prompt, ...prompts];
     prompt = "Test";
   }
 </script>
 
-<main>
+<main class="flex flex-col overflow-hidden h-full">
   <!-- Header -->
   <div class="h-16 flex items-center border-b px-4">
     <h1 class="text-xl flex-1">PromptManager</h1>
@@ -48,8 +47,35 @@
   </div>
   <!-- Header -->
 
+  <!-- PromptManager -->
+  <div class="flex-1 flex overflow-hidden min-h-0">
+    <ul
+      class="flex-1 overflow-y-auto divide-y"
+      style="min-width:30%; max-height:100%"
+    >
+      {#each prompts as prompt}
+        <li
+          class="px-2 py-2"
+          style="word-wrap: break-word; white-space: pre-line;"
+        >
+          {prompt}
+        </li>
+      {/each}
+    </ul>
+    <div class="p-4" style="max-width:50%; max-height:100%;">
+      {#if imageSource != ""}
+        <img
+          class="object-contain h-full w-full"
+          src={imageSource}
+          alt="Prompt Result"
+        />
+      {/if}
+    </div>
+  </div>
+  <!-- PromptManager -->
+
   <!-- Prompt Input -->
-  <div class="h-24 flex items-center px-4 space-x-4">
+  <div class="h-24 flex items-center px-4 space-x-4 border-t">
     <Textarea
       bind:value={prompt}
       class="flex-1"
@@ -63,32 +89,4 @@
     >
   </div>
   <!-- Prompt Input -->
-
-  <div class="">
-    <div class="flex space-x-4 px-4">
-      <div class="flex-1">
-        <ul class="overflow-y-scroll divide-y" style="min-width:30%;">
-          {#each prompts as prompt}
-            <li
-              class="py-2"
-              style="word-wrap: break-word; white-space: pre-line;"
-            >
-              {prompt}
-            </li>
-          {/each}
-        </ul>
-      </div>
-      <div class="flex" style="max-width:50%">
-        {#if imageSource != ""}
-          <div>
-            <img
-              class="aspect-square object-contain rounded-lg"
-              src={imageSource}
-              alt="Prompt Result"
-            />
-          </div>
-        {/if}
-      </div>
-    </div>
-  </div>
 </main>
